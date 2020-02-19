2f26fe5b4cb616e78088d4ab82a86cebdb72ab3a
DrPi101 <ken-worthington@protonmail.com>
2020-02-19 13:01:09 +0000

    Added JSON sort to class demo

--- class-2.py
+++ class-2.py
@@ -1,4 +1,6 @@
-drivers = {"Lewis Hamilton" : 100000, "Lando Norris": 33333, "Max Vers" : 23222}
+import json
+
+drivers = {"Alfie Noakes" : 100000, "John Stitch": 33333, "Barry McDermott" : 23222}
 
 class Worker(object):
     def __init__(self, name, pay):
@@ -16,14 +18,23 @@ class Worker(object):
         
 def proc_pay():
     for driver, sal in drivers.items():
-        fname = driver.split()[0]
+        fname = driver.split()[0] # use the first word from the key
         r=(fname)
         print(r)
         r = Worker(driver, sal)
-        r.set_pay(0.12)
+        r.set_pay(0.12) # increase the pay by 12%
         r.show_pay()
-        # update sal
+        print("+" * 12)
         new_sal = (r.get_pay())
-        drivers.update({driver : new_sal})
+        drivers.update({driver : new_sal}) # update the original 'dict'
    
 proc_pay()
+
+# Next - Sort the keys by way of using JSON
+
+with open ("drivers.json", "w") as jf:
+    json.dump(drivers, jf, indent =2,  sort_keys = True)
+
+# See output file, Barry is now BEFORE John. Good!
+
+
