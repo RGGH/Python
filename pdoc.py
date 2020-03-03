82089557b93e9156f46f5e50f16a8beb3aa847c6
Dr Pi <59804556+DrPi101@users.noreply.github.com>
2020-03-03 18:18:53 +0000

    works with 3 files ... nice

--- pdoc.py
+++ pdoc.py
@@ -1,18 +1,21 @@
 # pip install python-docx
 # pip install docxtpl
 
+import time
 from docxtpl import DocxTemplate
 
-tpl = DocxTemplate("template.docx")
+def mkw(n):
+    tpl = DocxTemplate("template.docx")
+    context = {1:{"title": "moo1", "company_name": "Dr Pi Red", "date": "2020-03-03"},
+                 2:{"title": "moo2", "company_name": "Dr Pi Blue", "date": "2020-03-03"},
+                   3:{"title": "moo3", "company_name": "Dr Pi Grey", "date": "2020-03-03"}
 
-context2 = {
-    "title": "Person 2",
-    "company_name": "Dr Pi Ltd",
-    "date": "2020-03-03",  
-}
+               }
+    
+    tpl.render(context[n])
+    tpl.save("%s.docx" %str(n))
 
-
-tpl.render(context2)
-
-for i in range (5):
-    tpl.save("t"+str(i)+".docx")
+for i in range(1,4):
+    mkw(i)
+    
+# https://stackoverflow.com/questions/39233973/get-all-keys-of-a-nested-dictionary
