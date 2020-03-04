3469ff6a5493dbf47716aff9ecc346195f815c23
Dr Pi <59804556+DrPi101@users.noreply.github.com>
2020-03-04 12:14:43 +0000

    added progress display to screen

--- pdoc.py
+++ pdoc.py
@@ -1,21 +1,26 @@
 # pip install python-docx
-# pip install docxtpl
+# pip install docxtpl <- Better for making new files from a template
 
+import random
 import time
+
 from docxtpl import DocxTemplate
 
 def mkw(n):
-    tpl = DocxTemplate("template.docx")
-    context = {1:{"title": "moo1", "company_name": "Dr Pi Red", "date": "2020-03-03"},
-                 2:{"title": "moo2", "company_name": "Dr Pi Blue", "date": "2020-03-03"},
-                   3:{"title": "moo3", "company_name": "Dr Pi Grey", "date": "2020-03-03"}
-
-               }
-    
+    tpl = DocxTemplate("template.docx") # In same directory
+    context = {1:{"title":"Alfie","company_name":"Dr Pi Red","date": "2020-03-03"},
+               2:{"title":"Derek","company_name":"Dr Pi Blue","date": "2020-03-03"},
+               3:{"title":"Norman","company_name":"Dr Pi Blue","date": "2020-03-03"},
+               4:{"title":"Clive","company_name":"Dr Pi Green","date": "2020-03-03"}
+               }  
     tpl.render(context[n])
     tpl.save("%s.docx" %str(n))
+    wait = time.sleep(random.randint(1,2))
 
-for i in range(1,4):
+# Main #
+
+for i in range(1,5):
+    print("Making file: ",f"{i}," ,"..Please Wait...")
     mkw(i)
-    
-# https://stackoverflow.com/questions/39233973/get-all-keys-of-a-nested-dictionary
+print("Done! - Now check your files")
+
