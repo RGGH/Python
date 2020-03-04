f2b826123dd32637e731ccc91aac4f1a6d7062b7
Dr Pi <59804556+DrPi101@users.noreply.github.com>
2020-03-04 19:34:27 +0000

    final

--- pdoc.py
+++ pdoc.py
@@ -1,3 +1,6 @@
+'''
+Dynamically generate word documents using data from a CSV - with 1 template file.
+'''
 # pip install python-docx
 # pip install docxtpl <- Better for making new files from a template
 import random
@@ -6,28 +9,29 @@ import csv
 import pandas as pd
 from docxtpl import DocxTemplate
 
+# Source CSV - column names that must match the *** that are {{***}} inside "template.docx"
 csvfn = "cnw.csv"
 
 def mkw(n):
     tpl = DocxTemplate("template.docx") # In same directory
     df = pd.read_csv(csvfn)
-    df_to_doct = df.to_dict()
-    print(df)
+    df_to_doct = df.to_dict() # dataframe -> dict for the template render
     x = df.to_dict(orient='records')
     context = x
-    
     tpl.render(context[n])
-    tpl.save("%s.docx" %str(n))
+    tpl.save("%s.docx" %str(n+1))
     
+    # Wait a random time - increase to (60,180) for real production run.
     wait = time.sleep(random.randint(1,2))
 
-# Main #
+#-------------------Main---------------------#
 
-df2 = len(pd.read_csv(csvfn)) - 1
-print (df2)
+df2 = len(pd.read_csv(csvfn)) 
+print ("There will be ", df2, "files")
 
-for i in range(1,df2):
+for i in range(0,df2):
     print("Making file: ",f"{i}," ,"..Please Wait...")
     mkw(i)
+    
 print("Done! - Now check your files")
 
