27f0b382675dc076fdb5d083396731496bfc9a6b
Dr Pi <59804556+DrPi101@users.noreply.github.com>
2020-03-04 14:01:52 +0000

    This reads a csv file called "cnw.csv" and produces word docs, using a template "template.docx"
    3 files are needed to make this run.
    
    pdoc.py
    template.docx
    cnw.csv ( with column names that match the {{****}} names inside the pairs of braces in the template.docx

--- pdoc.py
+++ pdoc.py
@@ -1,25 +1,32 @@
 # pip install python-docx
 # pip install docxtpl <- Better for making new files from a template
-
 import random
 import time
-
+import csv
+import pandas as pd
 from docxtpl import DocxTemplate
+
+csvfn = "cnw.csv"
 
 def mkw(n):
     tpl = DocxTemplate("template.docx") # In same directory
-    context = {1:{"title":"Alfie","company_name":"Dr Pi Red","date": "2020-03-03"},
-               2:{"title":"Derek","company_name":"Dr Pi Blue","date": "2020-03-03"},
-               3:{"title":"Norman","company_name":"Dr Pi Blue","date": "2020-03-03"},
-               4:{"title":"Clive","company_name":"Dr Pi Green","date": "2020-03-03"}
-               }  
+    df = pd.read_csv(csvfn)
+    df_to_doct = df.to_dict()
+    print(df)
+    x = df.to_dict(orient='records')
+    context = x
+    
     tpl.render(context[n])
     tpl.save("%s.docx" %str(n))
+    
     wait = time.sleep(random.randint(1,2))
 
 # Main #
 
-for i in range(1,5):
+df2 = len(pd.read_csv(csvfn)) - 1
+print (df2)
+
+for i in range(1,df2):
     print("Making file: ",f"{i}," ,"..Please Wait...")
     mkw(i)
 print("Done! - Now check your files")
