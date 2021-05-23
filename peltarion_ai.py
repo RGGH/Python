c252ac637ab48be2ece783be86c1703d058168df
Red and Green <62441426+RGGH@users.noreply.github.com>
2021-05-23 19:42:55 +0100

    Update peltarion_ai.py

--- peltarion_ai.py
+++ peltarion_ai.py
@@ -4,13 +4,15 @@ url = 'https://a.azure-eu-west.platform.peltarion.com/deployment/0b76101a-b5b8-4
 token = '907e31b5-51fd-4756-bdc4-8fa56d883c03'
 header = {'Authorization': 'Bearer {}'.format(token), 'Content-Type': 'application/json'}
 
-payload = '{"rows": [{"text": " huge explosion in italy "}]}'
+payload = '{"rows": [{"text": "England on fire in world cup victory"}]}'
 response = requests.request("POST", url, headers=header, data=payload)
 print(response.json())
 
 res = response.json()
 resf = float(res['rows'][0]['target'])
-if resf > 0.5:
+if resf > 0.7:
     print("Disaster")
+elif resf >0.55:
+    print("Not sure")   
 else:
     print("Not a disaster")
