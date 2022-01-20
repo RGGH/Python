0800931df58bdc04094fbaa89e17f01f20d8fce6
Red and Green <62441426+RGGH@users.noreply.github.com>
2022-01-20 21:47:12 +0000

    Create questions.py

---
+++ Challenges/questions.py
@@ -0,0 +1,58 @@
+#  Q1
+a = 1
+b = 0
+c = a & b # false
+print(c)
+d = a | b
+print(d)
+e = a ^ b
+print ( c + d + e)
+
+# # Q2
+# i = 0
+# while i <=5:
+#     i += 1
+#     if i % 2 == 0:
+#         break
+#     print("*")
+
+# # Q3
+# var = 1
+# while var < 10:
+#     print("#")
+#     var=var << 1
+#     #print(var)
+#     # << 	Zero fill left shift 	Shift left by pushing zeros in from the right and let the leftmost bits fall off
+
+# # Q4
+# i = 0
+# while i <=3:
+#     i +=2
+#     print("*")
+
+
+# ----
+# z=10
+# y=0
+
+# x = y<z and z>y or y>z and z<y
+# #x = true and false or false and false
+
+# print(x)
+# ----
+
+# # Q5
+# lst = [1,2,3]
+# for v in range(len(lst)):
+#     lst.insert(1,lst[v])
+# print(lst)
+
+# # Q6
+# lst = [0,1,2,3]
+
+# x = 1
+
+# for elem in lst:
+# 	print(x)
+# 	x *=elem
+# 	print(f"elem : {elem} x : {x}")
