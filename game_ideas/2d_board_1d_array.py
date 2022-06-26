1052c0404e022886ccd5722961d0497cf8058cf1
Red and Green <62441426+RGGH@users.noreply.github.com>
2022-06-26 20:33:36 +0100

    Update 2d_board_1d_array.py

--- game_ideas/2d_board_1d_array.py
+++ game_ideas/2d_board_1d_array.py
@@ -3,6 +3,11 @@
 #-------------------------
 # --- set up -------------
 #-------------------------
+
+
+from typing import List
+
+
 board =[9,9,9,9,9,9,9,9,9,9,
         9,0,0,0,0,0,0,0,0,9,
         9,0,0,0,0,0,0,0,0,9,
@@ -15,82 +20,141 @@ board =[9,9,9,9,9,9,9,9,9,9,
         9,9,9,9,9,9,9,9,9,9,
         ]
 
-rows = 8
-cols = 8
+ROWS = 8
+COLS = 8
 
-board_h = rows + 2
-board_w = cols + 2
+BOARD_H = ROWS + 2
+BOARD_W = COLS + 2
 
-row=0
-col=0
+ROW=0
+COL=0
 
-board_len = board_h * board_w
-move_counter = 1
+BOARD_LEN = BOARD_H * BOARD_W
+MOVE_COUNTER = 0
+
+PLAYER = 1
+AI = -1
+
+turn = 0
+
 #-------------------------
-def moves_left(board) :
-        
-    for i in range(board_h) :
-        for j in range(board_w) :
-            if (board[j+board_w*i] == 0) :
+#-------Functions --------
+#-------------------------
+
+
+def moves_left(brd) :
+    ''' check each square of every column of every row '''
+    for i in range(BOARD_H) :
+        for j in range(BOARD_W) :
+            if brd[j+BOARD_W*i] == 0 :
                 return True
     return False
 
-def print_board(board):
-    for i in range(board_h):
-        for j in range(board_w):
-            print(board[j+board_w*i], end = " ")
+def print_board(brd):
+    print("\n")
+    for i in range(BOARD_H):
+        for j in range(BOARD_W):
+            print(brd[j+BOARD_W*i], end = " ")
         print("-")
 
-def get_move(row,col,player,board):
-    """ get the row and column number from user """
+
+def evaluate() :
+    ''' heuristics '''
+    for i in range(1,BOARD_H):
+        for j in range(1,BOARD_W-1) :
+            if abs(board[j+(BOARD_W*i)])==1:
+
+                # Check rows - 3 in a row
+                if (board[j+(BOARD_W)*i]==board[j+(BOARD_W*i)+1] and board[j+(BOARD_W*i)+1] ==  board[j+(BOARD_W*i)+2]):
+                    if board[j+(BOARD_W)*i] == 1:
+                        return 30, PLAYER
+                    if board[j+(BOARD_W)*i] == 1:
+                        return -30, AI
+                
+                #2 Check rows - 2 in a row  
+                elif (board[j+(BOARD_W)*i]==board[j+(BOARD_W*i)+1]):
+                    if board[j+(BOARD_W)*i] == 1:
+                        return 20, PLAYER
+                    if board[j+(BOARD_W)*i] == -1:
+                        return -20, AI
+
+ 
+    for j in range(1,BOARD_H-1):
+        for j in range(1,BOARD_W-1) :
+
+        # 3 in a COL
+            if abs(board[j+(BOARD_W*i)])==1:
+                if board[(i*BOARD_W)+j]==1 or board[(i*BOARD_W)+j]==-1:
+                    if (board[(i*BOARD_W)+j]==board[(i+1)*BOARD_W+j]
+                    and board[(i+1)*(BOARD_W)+j] ==  board[(i+2)*(BOARD_W)+j]):
+                        if board[j+(BOARD_W)*i] == 1:
+                            return 30, PLAYER
+                        if board[j+(BOARD_W)*i] == -1:
+                            return -30, AI
+
+            # 2 in a COL
+            if abs(board[j+(BOARD_W*i)])==1:
+                if (board[(i*BOARD_W)+j]==board[(i+1)*BOARD_W+j]):
+                    if board[j+(BOARD_W)*i] == 1:
+                        return 20, PLAYER
+                    if board[j+(BOARD_W)*i] == -1:
+                        return -20, AI
+
+
+def get_move(rw:int,cl:int,turn:int,brd:List)->board:
+    ''' get the row and column number from user '''
+    
     user_choice = 0
-    print(f"\nPlayer {player} enter your choice \n")
+    print(f"\nPlayer {turn} enter your choice \n")
 
     # Get Row
-    while user_choice < 1 or user_choice > rows:
-        try:
-            user_choice = int(input(f"Please choose a row number between 1 and {rows} > "))
-        except ValueError:
-            print('We expect you to enter a valid integer')
-        row = user_choice
+
+    try:
+        user_choice = int(input(f"Please choose a row number between 1 and {ROWS} > "))
+    except ValueError:
+        print('We expect you to enter a valid integer')
+    rw = user_choice
 
     user_choice = 0
     # Get col
-    while user_choice < 1 or user_choice > cols:
-        try:
-            user_choice = int(input(f"Please choose a colum number between 1 and {rows} > "))
-        except ValueError:
-            print('We expect you to enter a valid integer')
-        col = user_choice
+    try:
+        user_choice = int(input(f"Please choose a column number between 1 and {ROWS} > "))
+    except ValueError:
+        print('We expect you to enter a valid integer')
+    cl = user_choice
 
-    pos = ((int(row)*board_w))+(int(col))
+    pos = ((int(rw)*BOARD_W))+(int(cl))
 
-    if player == 1: 
-        board[pos]=1
-    else:
-        board[pos]=2
+    if turn == PLAYER:
+        brd[pos]=1
+    if turn == AI:
+        brd[pos]=-1
 
-    return board
+    return brd
 
 # ------------ #
 # -- Start --- #
 # ------------ #
 
-while True:
+if __name__ == "__main__":
 
-    """ check if any moves available """
-    if moves_left(board)==False:
-        print("No moves left")
-        break
+    while True:
+        
+        if moves_left(board) is False:
+            print("No moves left")
+            break
 
-    """ run the game loop """
-    if move_counter % 2 != 0:
-        player = 1
-    else:
-        player = 2
-    move_counter+=1
+        if MOVE_COUNTER % 2 != 0:
+            turn = PLAYER
+        else:
+            turn = AI
 
-    get_move(row,col,player,board)
-    print_board(board)
+
+        # Get the move and redraw the board on screen
+        get_move(ROW,COL,turn,board)
+        print_board(board)
+        print(evaluate())
+
+        MOVE_COUNTER+=1
 
 # ------------ #
