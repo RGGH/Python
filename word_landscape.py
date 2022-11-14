62d3efd6e35ba2019a699dee37cc52b0ee23db87
Red and Green <62441426+RGGH@users.noreply.github.com>
2022-11-14 17:35:19 +0000

    Create word_landscape.py

---
+++ word_landscape.py
@@ -0,0 +1,58 @@
+from docx import Document
+from docx.shared import Inches
+
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='Intense Quote')
+
+document.add_paragraph(
+    'first item in unordered list', style='List Bullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='List Number'
+)
+
+
+records = (
+    (3, '101', 'Spam'),
+    (7, '422', 'Eggs'),
+    (4, '631', 'Spam, spam, eggs, and spam')
+)
+
+table = document.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+for qty, id, desc in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = str(qty)
+    row_cells[1].text = id
+    row_cells[2].text = desc
+
+# switch to Landscape # 
+
+from docx.enum.section import WD_ORIENT
+
+section = document.sections
+
+section.page_height = 8
+section.page_width = 14
+
+section = document.sections[-1]
+section.orientation = WD_ORIENT.LANDSCAPE
+new_width, new_height = section.page_height, section.page_width
+
+section.page_width = new_width
+section.page_height = new_height
+
+document.save('demo.docx')
